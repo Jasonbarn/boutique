5ebee02936830b636a20035bb4c6b4d4f7916691
jason <barnajason27@gmail.com>
2024-06-26 09:45:05 -0400

    migrations des données(user,category,product)

--- database/migrations/2024_06_26_132655_create_products_table.php
+++ database/migrations/2024_06_26_132655_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name', 60);
+            $table->text('description')->nullable();
+            $table->float('price')->nullable();
+            $table->string('image', 255)->nullable();
+            $table->integer('stock')->default(0); // quantité en stock
             $table->timestamps();
         });
     }
